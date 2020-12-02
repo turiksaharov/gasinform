d2e1cd5589b35361dfbade179fe90e40a505b0b6
turiksaharov <74906291+turiksaharov@users.noreply.github.com>
2020-12-02 19:16:56 +0300

    удален метод when

--- arthur2/src/main/java/Main.java
+++ arthur2/src/main/java/Main.java
@@ -24,7 +24,6 @@ public class Main {
                 .queryParam("lang", "ru")
                 .queryParam("appid", appidToken)
                 .get("/weather")
-//                .when()
                 .then()
                 .statusCode(200)
                 .extract()
